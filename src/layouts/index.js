3d4478387b67d1c66a5823a81ffba72113f5e112
Aditi Rajagopal <arajagopal@us.ibm.com>
2017-08-23 18:38:17 -0500

    Adding Meta Tags
    For facebook etc.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -38,10 +38,20 @@ const TemplateWrapper = ({ children }) =>
     <Helmet
       title="Gatsby Default Starter"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'Tangles Hair Extensions and More', content: 'Tangles Hair Extensions and More. Where genuinely caring about your hair is a wonderful experience' },
+        { name: 'keywords', content: 'hair, hair extensions, coloring, ombre, balayage, brazilian blowouts, keratin treatments' },
       ]}
     >
+    <meta property="og:title" content="Tangles Hair Extensions and More" />
+    <meta property="og:description" content="Tangles Hair Extensions and More. Where genuinely caring about your hair is a wonderful experience" />
+    <meta property="og:image" content={kellieLogo} />
+    <meta property="og:url" content="https://kellie-does-ur-hair.netlify.com/" />
+    <meta name="twitter:card" content="summary_large_image" />
+
+    <meta property="og:site_name" content="Tangles Hair Extensions and More" />
+    <meta name="twitter:image:alt" content="Tangles Hair Extensions and More" />
+
+    <meta property="fb:app_id" content="Tangles Hair Extensions and More" />
       <link rel="stylesheet" href="https://unpkg.com/tachyons@4.8.0/css/tachyons.min.css"/>
     </Helmet>
     <div
